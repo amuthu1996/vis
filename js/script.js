ca2e8819cd380eae04d9257624c160a695c20bcd
amuthu1996 <amuthu1996@gmail.com>
2016-10-04 23:33:57 +0530

    sets added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
 
-function myfunc(filename){
+function myfunc(filename, set){
 //Creating a new AJAX request that will request 'test.csv' from the current directory
       $(".article").load('articles/'+filename, function(responseTxt, statusTxt, xhr){
         if(statusTxt == "error")
             alert("Error: " + xhr.status + ": " + xhr.statusText);
       });
 
-      var img = $(".image").attr('src', 'images/' + filename + '.png').on('load', function() {
+      var img = $(".image").attr('src', 'images/' + set + '/' + filename + '.png').on('load', function() {
         if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
             alert('broken image!');
-        } else {
-              alert("Epic Failure...lol");
         }
     });
 }
 
 $(document).ready(function(){
   w3IncludeHTML();
-  myfunc('inp1.txt');
+  myfunc('inp1.txt','set1');
   $(".but").click(function(){
-    myfunc($('.dropdown').val());
+    myfunc($('.dropdown').val(), $('.set').val());
   });
 
 });
--- list.py
+++ list.py
@@ -1,9 +1,28 @@
 import os
 
-fileList = enumerate(sorted(os.listdir("./articles/")))
-filename = 'options.html'
-page = open(filename,'w')
-page.write('<select class="dropdown form-control">\n')
-for key,f in fileList:
-    page.write('\t<option value="' + f + '">' + f + '</option>\n')
-page.write('</select>')
+
+filename 	= 'options.html'
+setname 	= 'sets.html'
+articleDir 	= './articles/'
+imageDir 	= './images/'
+
+#file list with sorted input articles
+fileList 	= sorted(os.listdir(articleDir), cmp = None, key=lambda x : int(x[3:-4]))
+#set list with directory names
+setList 	= [name for name in os.listdir(imageDir) if os.path.isdir(os.path.join(imageDir, name))]
+setList.sort()
+
+with open(filename,'w') as page:
+	page.write('<select class="dropdown form-control">\n')
+	for f in fileList:
+	    page.write('\t<option value="' + f + '">' + f + '</option>\n')
+	page.write('</select>')
+	page.close()
+
+with open(setname,'w') as page:
+	page.write('<select class="set form-control">\n')
+	for f in setList:
+	    page.write('\t<option value="' + f + '">' + f + '</option>\n')
+	page.write('</select>')
+	page.close()
+
